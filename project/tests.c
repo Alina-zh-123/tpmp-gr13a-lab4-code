052cffc7cc6643f32c2ef3c1481a262347d5c33e
Alina-zh-123 <6817076@mail.ru>
2026-03-25 15:31:13 +0300

    Update tests.c

--- project/tests.c
+++ project/tests.c
@@ -2,19 +2,36 @@
 #include <stdio.h>
 #include "../includes/db.h"
 
+int Get_Flower_Price(sqlite3 *db, int id, double *price);
+
 void test_database_logic() {
     sqlite3 *db;
-    sqlite3_open(":memory:", &db);
+    int rc;
+
+    rc = sqlite3_open(":memory:", &db);
+    assert(rc == SQLITE_OK);
+    printf("Test 0: DB open OK\n");
+
     Create_Tables(db);
+    printf("Test 1: Create_Tables OK\n");
 
     Insert_Flower(db, "Test", "Sort", 10.0);
-    printf("Test 1: Insert_Flower - OK\n");
 
-    Safe_Price_Update(db, 1, 15.0); 
-    printf("Test 2: Safe_Price_Update Logic - OK\n");
+    double price = 0;
+    rc = Get_Flower_Price(db, 1, &price);
+    assert(rc == SQLITE_OK);
+    assert(price == 10.0);
+    printf("Test 2: Insert_Flower OK\n");
+
+    Safe_Price_Update(db, 1, 15.0);
+
+    rc = Get_Flower_Price(db, 1, &price);
+    assert(rc == SQLITE_OK);
+    assert(price == 10.0);
+    printf("Test 3: Safe_Price_Update OK\n");
 
     Delete_Order_By_Id(db, 999);
-    printf("Test 3: Delete_Order - OK\n");
+    printf("Test 4: Delete_Order OK\n");
 
     sqlite3_close(db);
 }
